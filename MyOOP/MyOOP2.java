4ce6f4933c35f3ebcfc9bdab61e470c876c9cf34
enter <enter@DESKTOP-0EFD5CU>
2021-03-18 21:08:12 +0900

    MYOOP

---
+++ MyOOP/MyOOP2.java
@@ -0,0 +1,16 @@
+public class MyOOP2 {
+	public static void main(String[] args) {
+		Print.delimiter = "----";
+		Print.A();
+		Print.A();
+		Print.B();
+		Print.B();
+		
+		Print.delimiter = "****";
+		Print.A();
+		Print.B();
+		Print.B();
+		Print.B();
+	}
+	
+}
---
+++ MyOOP/MyOOP3.java
@@ -0,0 +1,26 @@
+public class MyOOP3 {
+	public static void main(String[] args) {
+		Print p1 = new Print();
+		p1.delimiter = "***";
+		p1.A();
+		p1.A();
+		p1.B();
+		p1.B();
+		
+		
+		Print p2 = new Print();
+		p2.delimiter = "***";
+		p2.A();
+		p2.A();
+		p2.B();
+		p2.B();
+		
+		p1.A();
+		p2.A();
+		p1.A();
+		p2.A();
+		
+				
+	}
+	
+}
